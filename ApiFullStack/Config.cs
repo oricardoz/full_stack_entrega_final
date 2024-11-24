1ab98a865fb37f07212c60aa89c1a661b2b43b04
oricardoz <ricardoapzaghetto@gmail.com>
2024-11-23 23:31:40 -0300

    add: configurando variaveis de ambinte

--- ApiFullStack/Config.cs
+++ ApiFullStack/Config.cs
@@ -4,5 +4,14 @@ namespace ApiFullStack;
 
 public class Config
 {
+    public string ChavePrivada {get; set;} = string.Empty;
+
+    public static Config Instancia => instancia ??= new Config{
+        ChavePrivada = Environment.GetEnvironmentVariable("AUTH_CHAVE_PRIVADA")
+    };
+
+    private Config(){}
+
+    private static Config? instancia = null;
 
 }
